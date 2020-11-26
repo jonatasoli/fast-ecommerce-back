fb439ab8fa695f85b740cd53e71d80c9711fa947
Jônatas Oliveira <contato@jonatasoliveira.me>
2020-11-26 11:59:35 -0300

    api calc shipping

--- app/domains/domain_shipping.py
+++ app/domains/domain_shipping.py
@@ -3,20 +3,21 @@ from zipcode.zip_code import CalculateShipping, FindZipCode
 from dynaconf import settings
 
 def shipping_zip_code(shipping: Shipping):
-    try: 
-        shipping = CalculateShipping(
-            zip_code_source=settings.ZIP_CODE_SOURCE,
-            zip_code_target=shipping.zip_code_target,
-            weigth=shipping.weigth,
-            length=shipping.length,
-            heigth=shipping.heigth,
-            width=shipping.width,
-            diameter=shipping.diameter
-        )
-        shipping = shipping.calculate_shipping()
-        return shipping
-    except Exception as e:
-        raise e
+    return { "shipping": 1000}
+    # try: 
+    #     shipping = CalculateShipping(
+    #         zip_code_source=settings.ZIP_CODE_SOURCE,
+    #         zip_code_target=shipping.zip_code_target,
+    #         weigth=shipping.weigth,
+    #         length=shipping.length,
+    #         heigth=shipping.heigth,
+    #         width=shipping.width,
+    #         diameter=shipping.diameter
+    #     )
+    #     shipping = shipping.calculate_shipping()
+    #     return shipping
+    # except Exception as e:
+    #     raise e
 
 def adress_zip_code(shipping: Shipping, status_code=200):
     try:
--- app/endpoints/v1/shipping.py
+++ app/endpoints/v1/shipping.py
@@ -1,6 +1,6 @@
 from fastapi import Header, APIRouter, Depends
 from sqlalchemy.orm import Session
-from schemas.shipping_schema import Shipping
+from schemas.shipping_schema import Shipping, ShippingCalc
 from domains import domain_shipping 
 from endpoints.deps import get_db
 
@@ -15,3 +15,9 @@ def zip_code_shipping(shipping_data: Shipping):
 def zip_code_adress(shipping_data: Shipping):
     adress = domain_shipping.adress_zip_code(shipping=shipping_data)
     return adress
+
+@shipping.post('/zip_code/shipping/calc', status_code=200)
+def zip_code_shipping(shipping_data: ShippingCalc):
+    from loguru import logger
+    logger.debug(shipping_data)
+    return { "shipping": 1000}
