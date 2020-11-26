f4d4bdf6ae0e6ebd3b243870780397c20d25ba7f
Thais Martins <thaismartins1999@gmail.com>
2020-11-25 21:45:07 -0300

    add tests

--- app/gateway/tests/test_gateway.py
+++ app/gateway/tests/test_gateway.py
@@ -1,5 +1,5 @@
 from gateway.payment_gateway import return_transaction
 
 def test_return_transaction():
-    transaction = return_transaction(10460784)
-    assert transaction.get("gateway_id") == 10460784
+    transaction = return_transaction(10517987) 
+    assert transaction.get("gateway_id") == 10517987
