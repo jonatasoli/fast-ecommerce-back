c19106e27019f3c332ea9ccf70df473e85b27ee7
Jonatas Oliveira <contato@jonatasoliveira.me>
2021-03-09 09:47:13 -0300

    readd tracking number table

---
+++ app/migrations/versions/425e147e716b_readd_tracking_number.py
@@ -0,0 +1,24 @@
+"""readd tracking number
+
+Revision ID: 425e147e716b
+Revises: e39f5d088828
+Create Date: 2021-03-09 09:46:03.647929
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '425e147e716b'
+down_revision = 'e39f5d088828'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
