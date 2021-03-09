fcbc7c9be70e2bfaef74d02963adb20524eee775
Jonatas Oliveira <contato@jonatasoliveira.me>
2021-03-09 09:42:45 -0300

    remove column tracking

---
+++ app/migrations/versions/e39f5d088828_remove_tracking_number.py
@@ -0,0 +1,24 @@
+"""remove tracking number
+
+Revision ID: e39f5d088828
+Revises: 5c8ee9bab134
+Create Date: 2021-03-09 09:42:14.547372
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e39f5d088828'
+down_revision = '5c8ee9bab134'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
