cd9c2633d94d9fe2bedf58ee91a7bdc753b55ad5
Jônatas Luiz de Oliveira <contact@jonatasoliveira.dev>
2023-09-11 12:26:26 +0200

    chore: troubleshooting ampq url

--- app/infra/worker.py
+++ app/infra/worker.py
@@ -1,4 +1,6 @@
+from loguru import logger
 from propan.fastapi import RabbitRouter
 from config import settings
 
+logger.info(f'{settings.BROKER_URL}')
 task_message_bus = RabbitRouter(settings.BROKER_URL)
