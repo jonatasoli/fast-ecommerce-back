7f08126ab203822bf68c1135ee93ddc84b6b4401
Jonatas Oliveira <contact@jonatasoliveira.dev>
2023-11-02 11:16:45 +0100

    feat: add checktoken route

--- app/user/services.py
+++ app/user/services.py
@@ -1,9 +1,44 @@
-from app.entities.user import UserCouponResponse
+from typing import Any
+from app.entities.user import UserCouponResponse, CredentialError
 from app.infra.models.users import User
+from jose import JWTError, jwt
+from config import settings
 
 
-def get_affiliate_urls(user: User, base_url: str):
+def get_affiliate_urls(user: User, base_url: str) -> UserCouponResponse:
+    """Get affiliate user and return code urls."""
+    _ = user
     _urls = []
-    for id in range(10):
-        _urls.append(f'{base_url}/?affiliate={id}')
+    for _id in range(10):
+        _urls.append(f'{base_url}/?affiliate={_id}')
     return UserCouponResponse(urls=_urls)
+
+
+def verify_token_user_is_valid(token: str, bootstrap: Any) -> None:
+    """Verify if token user is valid."""
+    user = bootstrap.user.get_current_user(token)
+    if not user:
+        raise CredentialError
+
+
+def get_current_user(
+    token: str,
+    bootstrap: Any,
+) -> User:
+    """Must return user db."""
+    try:
+        payload = jwt.decode(
+            token,
+            settings.SECRET_KEY,
+            algorithms=[settings.ALGORITHM],
+        )
+        document = payload.get('sub')
+        if document is None:
+            raise CredentialError
+    except JWTError:
+        raise CredentialError from JWTError
+
+    user = bootstrap.user_uow.get_user_by_document(document=document)
+    if user is None:
+        raise CredentialError
+    return user
