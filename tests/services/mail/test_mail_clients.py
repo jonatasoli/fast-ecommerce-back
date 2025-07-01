7ceff72e7759eaf724cce02fc384833d22f7f81d
Jonatas Oliveira <contact@jonatasoliveira.dev>
2025-07-01 15:53:36 +0200

    test: fix sendgrind mail tests

--- tests/services/mail/test_mail_clients.py
+++ tests/services/mail/test_mail_clients.py
@@ -1,22 +1,30 @@
 from faker import Faker
 import pytest
-from sendgrid import Mail
 
+from app.entities.mail import MailMessage
 from app.infra.constants import MailGateway
+from app.infra.crypto_tools import decrypt, encrypt
 from app.mail.services import send_mail
+from config import settings
+
 
 fake = Faker()
 
 def test_send_email_with_sendgrid(mocker):
     """Must raise email with sendgrid."""
     # Setup
+    key = settings.CAPI_SECRET
+    credential = encrypt(
+        message = b'{"secret": "SENDGRID_SECRET"}',
+        key=key,
+    )
     mock_session = mocker.MagicMock()
     mock_db = lambda: mock_session # noqa: E731
-    settings = mocker.MagicMock()
-    settings.provider = MailGateway.sendgrid
-    settings.credentials = {"secret": "SENDGRID_SECRET"}
+    _settings = mocker.MagicMock()
+    _settings.provider = MailGateway.sendgrid
+    _settings.credentials = credential.decode('utf-8')
     mock_session.begin.return_value.__enter__.return_value = mock_session
-    mock_session.scalar.return_value = settings
+    mock_session.scalar.return_value = _settings
 
     fake_response = mocker.Mock()
     fake_response.status_code = 202
@@ -24,10 +32,13 @@ def test_send_email_with_sendgrid(mocker):
     fake_response.headers = {"X-Test": "Header"}
     mock_send = mocker.patch("sendgrid.SendGridAPIClient.send", return_value=fake_response)
 
-    message = Mail(
+    message = MailMessage(
         from_email=fake.email(),
         to_emails=fake.email(),
-        subject=fake.sentence(nb_words=6).rstrip('.'))
+        subject=fake.sentence(nb_words=6).rstrip('.'),
+        plain_text_content=None,
+        html_content=fake.text(),
+    )
 
     # Act
     send_mail(message, db=mock_db)
@@ -39,12 +50,18 @@ def test_send_email_with_sendgrid(mocker):
 def test_send_email_fallback_legacy(mocker):
     """Must raise email with legacy."""
     # Setup
+    key = settings.CAPI_SECRET
+    credential = encrypt(
+        message = b'{"provider": "legacy"}',
+        key=key,
+    )
     mock_session = mocker.MagicMock()
     mock_db = lambda: mock_session # noqa: E731
-    settings = mocker.MagicMock()
-    settings.provider = "legacy"
+    _settings = mocker.MagicMock()
+    _settings.provider = "legacy"
+    _settings.credentials = credential.decode('utf-8')
     mock_session.begin.return_value.__enter__.return_value = mock_session
-    mock_session.scalar.return_value = settings
+    mock_session.scalar.return_value = _settings
 
     fake_response = mocker.Mock()
     fake_response.status_code = 202
@@ -52,10 +69,13 @@ def test_send_email_fallback_legacy(mocker):
     fake_response.headers = {"X-Test": "Header"}
     mock_send = mocker.patch("sendgrid.SendGridAPIClient.send", return_value=fake_response)
 
-    message = Mail(
+    message = MailMessage(
         from_email=fake.email(),
         to_emails=fake.email(),
-        subject=fake.sentence(nb_words=6).rstrip('.'))
+        subject=fake.sentence(nb_words=6).rstrip('.'),
+        plain_text_content=None,
+        html_content=fake.text(),
+    )
 
     # Act
     send_mail(message, db=mock_db)
@@ -68,13 +88,18 @@ def test_send_email_fallback_legacy(mocker):
 def test_send_email_raises_on_failure(mocker):
     """Must raise email exception."""
     # Setup
+    key = settings.CAPI_SECRET
+    credential = encrypt(
+        message = b'{"secret": "SENDGRID_SECRET"}',
+        key=key,
+    )
     mock_session = mocker.MagicMock()
     mock_db = lambda: mock_session # noqa: E731
-    settings = mocker.MagicMock()
-    settings.provider = MailGateway.sendgrid
-    settings.credentials = {"secret": "SENDGRID_SECRET"}
+    _settings = mocker.MagicMock()
+    _settings.provider = MailGateway.sendgrid
+    _settings.credentials = credential.decode('utf-8')
     mock_session.begin.return_value.__enter__.return_value = mock_session
-    mock_session.scalar.return_value = settings
+    mock_session.scalar.return_value = _settings
 
     fake_response = mocker.Mock()
     fake_response.status_code = 500
@@ -82,10 +107,13 @@ def test_send_email_raises_on_failure(mocker):
     fake_response.headers = {"X-Test": "Header"}
     mocker.patch("sendgrid.SendGridAPIClient.send", return_value=fake_response)
 
-    message = Mail(
+    message = MailMessage(
         from_email=fake.email(),
         to_emails=fake.email(),
-        subject=fake.sentence(nb_words=6).rstrip('.'))
+        subject=fake.sentence(nb_words=6).rstrip('.'),
+        plain_text_content=None,
+        html_content=fake.text(),
+    )
 
     # Act / Assert
     with pytest.raises(Exception): # noqa: B017
